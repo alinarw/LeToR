c895e826b2dc590e9be541a71069d9d3afce96df
Alina Dev <a@alinadev.com>
2018-09-24 14:10:08 -0400

    LeToR closed form and sgd solutions

---
+++ letor.py
@@ -0,0 +1,142 @@
+import numpy as np
+import time
+import random
+
+np.random.seed(42)
+random.seed(42)
+
+def import_data(file_path):
+    data_target = []
+    data_input = []
+    with open(file_path, 'r') as f:
+        while True:
+            data_str = f.readline().split(' ')
+            if not data_str[0]:
+                break
+            data_target.append(float(data_str[0]))
+            data_input.append([float(a.split(':')[1]) for a in data_str[2:48]])
+
+    x = np.array(data_input)
+    y = np.array(data_target).reshape((-1, 1))
+
+    return x, y
+
+def phi(x, mu, sigma):
+
+    l = len(mu)
+    res = np.empty((x.shape[0], l))
+    for i in range(l):
+        arg = x - mu[i]
+        sigma_inv = np.linalg.pinv(sigma[i])
+        for j in range(x.shape[0]):
+            tmp = arg[j, :].reshape((-1, 1))
+            res[j, i] = np.exp(tmp.T.dot(sigma_inv).dot(tmp))
+    return res
+
+def linear(x, w, mu, sigma):
+    p = phi(x, mu, sigma)
+    p = np.hstack((np.ones((p.shape[0], 1)), p))
+    return p.dot(w)
+
+def loss(y, p):
+    tmp = y - p
+    return tmp.T.dot(tmp) / 2
+
+def reg(w, lamb):
+    return lamb * w.T.dot(w)
+
+def rmse(y, p, w, lamb, n):
+    return np.sqrt(2 * (loss(y, p) + reg(w, lamb)) / n)
+
+def mu_simple(x, l):
+    idx = random.sample(list(range(0, x.shape[0])), l)
+    mu = []
+    for i in idx:
+        mu.append(x[i, :])
+    return mu
+
+def sigma_simple(x, l):
+    var = 1 + np.var(x, axis=0) / 10
+    sigma1 = np.diag(var)
+    return [sigma1] * l
+
+def closed_form_sol(x, y, mu, sigma, lamb):
+    Phi = phi(x, mu, sigma)
+    Phi = np.hstack((np.ones((Phi.shape[0], 1)), Phi))
+    tmp1 = np.diag(np.ones(len(mu) + 1) * lamb)
+    tmp2 = Phi.T.dot(Phi)
+    tmp = np.linalg.inv(tmp1 + tmp2)
+    w_sol = tmp.dot(Phi.T).dot(y)
+
+    return w_sol
+
+def sgd(x, y, lamb, lr_low, lr_high, mu, sigma):
+    w = np.zeros((len(mu) + 1, 1))
+    print(w.shape)
+    Phi = phi(x, mu, sigma)
+    Phi = np.hstack((np.ones((Phi.shape[0], 1)), Phi))
+    print(Phi.shape)
+    print(Phi[9, :].reshape((-1, 1)).shape)
+    n = x.shape[0]
+    for i in range(x.shape[0]):
+        lr = lr_decay(lr_low, lr_high, n, i)
+        if i % 100:
+            print(lr)
+        tmp1 = w.T.dot(Phi[i, :])
+        tmp = (y[i, :] - tmp1) * (Phi[i, :].reshape((-1, 1)))
+        loss_grad = (lamb*w - tmp)
+        w -= lr*loss_grad
+    return w
+
+def lr_decay(low, high, n, t):
+    lr = high + (low - high) * t / n
+    return lr
+
+
+if __name__ == '__main__':
+    # Extract feature values and labels from the data
+    file_path = r'path\to\file\Querylevelnorm.txt'
+    x, y = import_data(file_path)
+    #x = np.hstack((np.ones((x.shape[0], 1)), x))
+    n, m = x.shape
+
+    # Data partition
+    n_train = int(0.8 * n)
+    n_valid = int((n - n_train) / 2)
+
+    x_train = x[:n_train, :]
+    x_valid = x[n_train:n_train + n_valid, :]
+    x_test = x[n_train + n_valid:, :]
+
+    y_train = y[:n_train, :]
+    y_valid = y[n_train:n_train + n_valid, :]
+    y_test = y[n_train + n_valid:, :]
+
+    l = 10
+    lamb = 1
+    lr_low = 10e-17
+    lr_high = 10e-10
+
+    mu = mu_simple(x_train, l)
+    sigma = sigma_simple(x_train, l)
+    #sigma = [np.random.uniform(0, 1, (46,46)), np.random.uniform(0, 1, (46,46))]
+
+    w0 = np.random.uniform(0, 1, (len(mu)+1, 1))
+    p0 = linear(x_test, w0, mu, sigma)
+    rmse0 = rmse(y_test, p0, w0, lamb, y_test.shape[0])
+    print(rmse0)
+
+    w = closed_form_sol(x_train, y_train, mu, sigma, lamb)
+
+    p = linear(x_test, w, mu, sigma)
+    print(w)
+    rmse0 = rmse(y_test, p, w, lamb, y_test.shape[0])
+    print(rmse0)
+
+    # SGD
+    # w = sgd(x_train, y_train, lamb, lr_low, lr_high, mu, sigma)
+    #
+    # p = linear(x_test, w, mu, sigma)
+    # print(w)
+    # rmse0 = rmse(y_test, p, w, lamb, y_test.shape[0])
+    # print(rmse0)
